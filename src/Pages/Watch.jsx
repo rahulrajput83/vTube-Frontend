5984cac60d9fad2649d09d13d9bd5d21a68d9ab1
rahulrajput83 <rahulrajput21112@gmail.com>
2022-09-30 19:25:17 +0530

    updated watch page

--- src/Pages/Watch.jsx
+++ src/Pages/Watch.jsx
@@ -20,12 +20,12 @@ function Watch() {
 
 
   return (
-    <div className='w-full p-1 text-white grid gap-x-5 gap-y-6 grid-cols-1 sm:grid-cols-3 justify-center items-start'>
+    <div className='w-full p-1 text-white grid gap-x-0 sm:gap-x-4  gap-y-6 grid-cols-1 sm:grid-cols-3 justify-center items-start'>
       <div className='col-span-2 mx-0 my-1 md:mx-4 flex flex-col h-fit'>
         {video.length > 0 ?
-          <Video video={video} /> : <span>Err</span>}
+          <Video video={video} /> : <span></span>}
       </div>
-      <div className='flex flex-col mx-auto justify-center items-start'>
+      <div className='flex flex-col px-1 justify-center items-start'>
         {data.map((item, index) => (
           <Recommendation key={index} item={item} index={index}/>
         ))}
